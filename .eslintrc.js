2779e6889eb8f9c7e54b9659d43d7cbd505fd11d
Adam <adam.holo@mail.ru>
2022-07-28 16:10:14 +0400

    feat: add eslint and alias

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "prettier",
+        "react"
+    ],
+    "rules": {
+        "prettier/prettier": 1
+    },
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    }
+}
