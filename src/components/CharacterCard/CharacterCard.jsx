b28586075497c59ebbf907b4ca483af16f1e631a
Adam <adam.holo@mail.ru>
2022-07-29 17:57:05 +0400

    feat: add CharacterCard component

---
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import {
+  CharacterDescription,
+  CharacterImage,
+  CharacterWrapper,
+} from "./CharacterCard.style";
+
+const CharacterCard = ({ character }) => {
+  const { name, image, species } = character;
+
+  return (
+    <CharacterWrapper>
+      <CharacterImage image={image} />
+
+      <CharacterDescription>
+        <h6 className="character__name">{name}</h6>
+        <h6 className="character__species">{species}</h6>
+      </CharacterDescription>
+    </CharacterWrapper>
+  );
+};
+
+CharacterCard.propTypes = {
+  character: {
+    name: PropTypes.string,
+    image: PropTypes.string,
+    species: PropTypes.string,
+  },
+};
+export default CharacterCard;
