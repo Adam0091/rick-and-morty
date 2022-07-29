f9fe18db5d46c86ee169646412b953932f302c1a
Adam <adam.holo@mail.ru>
2022-07-29 17:58:22 +0400

    feat: add new alies

--- config-overrides.js
+++ config-overrides.js
@@ -3,8 +3,10 @@ const { alias } = require("react-app-rewire-alias");
 module.exports = function override(config, env) {
   alias({
     "@components": "src/components",
+    "@constants": "src/constants",
     "@containers": "src/containers",
     "@routes": "src/routes",
+    "@utils": "src/utils",
   })(config);
 
   return config;
