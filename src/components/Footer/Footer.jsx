61990c845ca370f34695410c5b45296acda7a7c0
Adam <adam.holo@mail.ru>
2022-07-29 17:53:19 +0400

    feat: add Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import { FooterWrapper } from "./Footer.style";
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <h3>Make with ❤️ for the MobProgramming team</h3>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
