7616d694d94fad4b061d6c757e277bc0f7cabdc2
Adam <adam.holo@mail.ru>
2022-08-04 15:04:55 +0400

    fix: change craco config and add homepage in package.json

--- craco.config.js
+++ craco.config.js
@@ -1,23 +1,14 @@
-const path = require('path');
-const CracoAlias = require("craco-alias");
+const CracoAlias = require('craco-alias')
 
 module.exports = {
-  plugins: [{
-    plugin: CracoAlias,
-    options: {
-      source: "options",
-      baseUrl: "./",
-      aliases: {
-        '@': './src',
-        "@components": './src/components',
-        "@containers": './src/containers',
-        "@constants": './src/constants',
-        "@routes": './src/routes',
-        "@utils": './src/utils',
-        "@types": './src/types',
-        "@assets": './src/assets',
-        "@UI-Kit": './src/components/UI-Kit'
-      }
-    }
-  }]
-};
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: '.',
+        tsConfigPath: './tsconfig.path.json',
+      },
+    },
+  ],
+}
