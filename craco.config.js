2d15fe34afcbe5f63aa2e63b80a92ff7cab8fabf
Adam <adam.holo@mail.ru>
2022-08-04 14:13:41 +0400

    fix: change config for craco

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,23 @@
 const path = require('path');
+const CracoAlias = require("craco-alias");
+
 module.exports = {
-  webpack: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      "@components": path.resolve(__dirname, 'src/components/'),
-      "@containers": path.resolve(__dirname, 'src/containers/'),
-      "@constants": path.resolve(__dirname, 'src/constants/'),
-      "@routes": path.resolve(__dirname, 'src/routes/'),
-      "@utils": path.resolve(__dirname, 'src/utils/'),
-      "@types": path.resolve(__dirname, 'src/types/'),
-      "@assets": path.resolve(__dirname, 'src/assets/'),
-      "@UI-Kit": path.resolve(__dirname, 'src/components/UI-Kit/')
-    },
-  },
+  plugins: [{
+    plugin: CracoAlias,
+    options: {
+      source: "options",
+      baseUrl: "./",
+      aliases: {
+        '@': './src',
+        "@components": './src/components',
+        "@containers": './src/containers',
+        "@constants": './src/constants',
+        "@routes": './src/routes',
+        "@utils": './src/utils',
+        "@types": './src/types',
+        "@assets": './src/assets',
+        "@UI-Kit": './src/components/UI-Kit'
+      }
+    }
+  }]
 };
