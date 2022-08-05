ed09145f605419b34a0d3f34b348c04eafbf1d4b
Adam <adam.holo@mail.ru>
2022-08-05 16:18:31 +0400

    feat: add GET_LOCATIONS

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -41,3 +41,20 @@ export const GET_CHARACTER = gql`
     }
   }
 `;
+
+export const GET_LOCATIONS = gql`
+  query LocationsList($ids: [ID!]!) {
+    locationsByIds(ids: $ids) {
+      id
+      name
+      type
+      dimension
+      residents {
+        id
+        name
+        type
+      }
+      created
+    }
+  }
+`;
